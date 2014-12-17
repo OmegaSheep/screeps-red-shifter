c3f56eb456a48646a63b4fc1b6762d3af6a6142f
Martin H <n17r4m@gmail.com>
2014-12-17 15:06:17 -0700

    redux

---
+++ math.js
@@ -0,0 +1,20 @@
+module.exports = (function math(){
+
+
+	/*
+	So, perhaps there should be some way to do something like the following
+	
+	
+	find new basis given n objectives across s vectors.
+	
+	
+	
+	The weight of the chance of the creep to go towards a target
+	is thresholded by a n dimensional trixie distribution across velocity
+	
+	Game.memory.creeps.me.lastVectors = [{v: {x,y}, u: {dx,dy}}]
+
+	
+	*/
+
+})()
