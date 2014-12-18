ee3c0d32a496f01ad4220dfe842ff661a5633659
Martin H <n17r4m@gmail.com>
2014-12-17 17:29:05 -0700

    logixxx

--- redux/creep_wisp.js
+++ redux/creep_wisp.js
@@ -2,7 +2,11 @@ module.exports = function Wisp(creep) {
 
 	var target = creep.pos.findNearest(Game.MY_CREEPS, {filter: function(creep) { return creep.hits < creep.hitsMax } } );
 	var enemy = creep.pos.findNearest(Game.HOSTILE_CREEPS);
-	var far = !creep.pos.inRangeTo(enemy, 3);
+	if(enemy){
+		var far = !creep.pos.inRangeTo(enemy, 3);
+	} else {
+		var far = true;
+	}
 
 	if(far && creep.body[1].hits > 0) {
 		creep.moveTo(target);
